9de1dfe1a12b45f0f79819e8c7d7e63cf1a5d1ba
Ryan Hill <ryan@ryanhill.com>
2025-08-12 08:20:35 -0500

    updates

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function jsonToURLEncoded(object: Record<string, any>) {
-  const queries = [];
-  for (const key in object) {
-    let value = object[key];
-    if (Array.isArray(value)) {
-      value = value.join(' ');
-    }
-
-    queries.push(key + '=' + value);
-  }
-  return queries.join('&');
+  return Object
+    .entries(object)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
 }
